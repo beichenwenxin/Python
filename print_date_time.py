ae9f42f370823fa019c1857f9e2259651db53591
file_shell <37932224+beichenwenxin@users.noreply.github.com>
2018-08-24 10:05:04 +0800

    Create print_date_time.py
    This script has a function that outputs different dates and formats functions, but this function needs to pass in a string as a parameter. If you do not want to pass in parameters, please pass in an empty string. There are many formats in the script annotation. Please read the script carefully.

---
+++ print_date_time.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import time
+
+def work_out(str):
+    #2018-08-24 09:54:44 Test string. 
+    print time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),str
+    
+    #20180824100107 Test string.
+    print time.strftime("%Y%m%d%H%M%S", time.localtime()),str
+    
+    #2018/08/24 10:01:07 Test string.
+    print time.strftime("%Y/%m/%d %H:%M:%S", time.localtime()),str
+    
+    #2018年08月24日 10:01:07 Test string.
+    print time.strftime("%Y年%m月%d日 %H:%M:%S", time.localtime()),str
+    
+    #Fri Aug 24 10:01:07 2018 Test string.
+    print time.strftime("%a %b %d %H:%M:%S %Y", time.localtime()),str
+
+if __name__ == '__main__':
+    str="Test string. "
+    work_out(str)
+    
+    work_out("")
+    
