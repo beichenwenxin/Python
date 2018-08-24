e55213e62438dfe5afc0c2b3ba0948c4f8b58244
file_shell <37932224+beichenwenxin@users.noreply.github.com>
2018-08-24 10:22:27 +0800

    Update print_date_time.py

--- print_date_time.py
+++ print_date_time.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+"""
+Author beichenwenxin https://github.com/beichenwenxin/Python/edit/master/print_date_time.py
+Created on Fri Aug 24 10:01:07 2018 
+"""
 import time
 
 def work_out(str):
